8ac05331ca7d206a5a630ac8ba590b8a86feed81
Adrian-26-Isotope <addi_z@web.de>
2021-07-08 19:01:02 +0200

    created PokemonSpeciesFacade
    it required some changes to the pokemonspecies model class

--- src/com/lodenrogue/pokeapiv2/PokeApiTestMain.java
+++ src/com/lodenrogue/pokeapiv2/PokeApiTestMain.java
@@ -13,6 +13,7 @@ import com.lodenrogue.pokeapiv2.model.contests.ContestEffect;
 import com.lodenrogue.pokeapiv2.model.contests.ContestType;
 import com.lodenrogue.pokeapiv2.model.contests.SuperContestEffect;
 import com.lodenrogue.pokeapiv2.model.encounters.EncounterMethod;
+import com.lodenrogue.pokeapiv2.model.pokemon.PokemonSpecies;
 import com.lodenrogue.pokeapiv2.model.utility.Language;
 import com.lodenrogue.pokeapiv2.service.berries.BerryFacade;
 import com.lodenrogue.pokeapiv2.service.berries.BerryFirmnessFacade;
@@ -21,79 +22,88 @@ import com.lodenrogue.pokeapiv2.service.contests.ContestEffectFacade;
 import com.lodenrogue.pokeapiv2.service.contests.ContestTypeFacade;
 import com.lodenrogue.pokeapiv2.service.contests.SuperContestEffectFacade;
 import com.lodenrogue.pokeapiv2.service.encounter.EncounterMethodFacade;
+import com.lodenrogue.pokeapiv2.service.pokemon.PokemonSpeciesFacade;
 import com.lodenrogue.pokeapiv2.service.utility.LanguageFacade;
 
 public class PokeApiTestMain {
 
-	public static void main(String[] args) throws JsonParseException, JsonMappingException, IOException {
-		testBerry();
-	}
+  public static void main(final String[] args) throws JsonParseException, JsonMappingException, IOException {
+    testPokemonSpecies();
+  }
 
-	private static void testEncounterMethod() {
-		EncounterMethod method = new EncounterMethodFacade().find("old-rod");
-		System.out.println(method.getNames().get(1).getName());
-	}
+  private static void testPokemonSpecies() {
+    PokemonSpecies pokemonSpecies = new PokemonSpeciesFacade().find("ivysaur");
+    assert pokemonSpecies.getId() == 2;
+    System.out.println(pokemonSpecies.getId());
+    System.out.println(pokemonSpecies.getName());
+    System.out.println(pokemonSpecies.getCaptureRate());
+  }
 
-	private static void testSuperContestEffect() {
-		SuperContestEffect effect = new SuperContestEffectFacade().find(4);
-		System.out.println(effect.getFlavorTextEntries().get(0).getFlavorText());
-	}
+  private static void testEncounterMethod() {
+    EncounterMethod method = new EncounterMethodFacade().find("old-rod");
+    System.out.println(method.getNames().get(1).getName());
+  }
 
-	private static void testContestEffect() {
-		ContestEffect effect = new ContestEffectFacade().find(4);
-		System.out.println(effect.getEffectEntries().get(0).getEffect());
-	}
+  private static void testSuperContestEffect() {
+    SuperContestEffect effect = new SuperContestEffectFacade().find(4);
+    System.out.println(effect.getFlavorTextEntries().get(0).getFlavorText());
+  }
 
-	private static void testContestType() {
-		ContestType type = new ContestTypeFacade().find("tough");
-		System.out.println(type.getNames().get(0).getName());
-	}
+  private static void testContestEffect() {
+    ContestEffect effect = new ContestEffectFacade().find(4);
+    System.out.println(effect.getEffectEntries().get(0).getEffect());
+  }
 
-	private static void testBerryFlavor() {
-		BerryFlavor flavor = new BerryFlavorFacade().find(4);
-		System.out.println(flavor.getBerries().get(0).getPotency());
-	}
+  private static void testContestType() {
+    ContestType type = new ContestTypeFacade().find("tough");
+    System.out.println(type.getNames().get(0).getName());
+  }
 
-	private static void testBerryFirmness() {
-		BerryFirmness firmness = new BerryFirmnessFacade().find("very-soft");
-		System.out.println(firmness.getBerries().get(0).getName());
+  private static void testBerryFlavor() {
+    BerryFlavor flavor = new BerryFlavorFacade().find(4);
+    System.out.println(flavor.getBerries().get(0).getPotency());
+  }
 
-	}
+  private static void testBerryFirmness() {
+    BerryFirmness firmness = new BerryFirmnessFacade().find("very-soft");
+    System.out.println(firmness.getBerries().get(0).getName());
 
-	private static void testBerry() {
-		Berry berry = new BerryFacade().find(2);
-		System.out.println(berry.getSmoothness() == 25);
-		System.out.println(berry.getName());
-		System.out.println(berry.getFlavors().get(0).getFlavor().getName());
-	}
+  }
 
-	/**
-	 * 
-	 */
-	private static void testPrimitiveArray() {
-		String json = "{\"possible_values\": [0, 5, 10, 15, 20, 25, 30]}";
+  private static void testBerry() {
+    Berry berry = new BerryFacade().find(2);
+    System.out.println(berry.getSmoothness() == 25);
+    System.out.println(berry.getName());
+    System.out.println(berry.getFlavors().get(0).getFlavor().getName());
+  }
 
-		ObjectMapper mapper = new ObjectMapper();
-		try {
-			TestClass test = mapper.readValue(json, TestClass.class);
-			System.out.println(Arrays.toString(test.getPossibleValues()));
-		}
-		catch (IOException e) {
-			// TODO Auto-generated catch block
-			e.printStackTrace();
-		}
-	}
+  /**
+   *
+   */
+  private static void testPrimitiveArray() {
+    String json = "{\"possible_values\": [0, 5, 10, 15, 20, 25, 30]}";
 
-	/**
-	 * 
-	 */
-	private static void testLanguage() {
-		Language language = new LanguageFacade().find(9);
+    ObjectMapper mapper = new ObjectMapper();
+    try {
+      TestClass test = mapper.readValue(json, TestClass.class);
+      System.out.println(Arrays.toString(test.getPossibleValues()));
+    }
+    catch (IOException e) {
+      // TODO Auto-generated catch block
+      e.printStackTrace();
+    }
+  }
 
-		System.out.print(language.getId() + " " + language.getName() + " ");
-		language.getNames().forEach(n -> {
-			System.out.print(n.getName() + " ");
-		});
-	}
+  /**
+   *
+   */
+  private static void testLanguage() {
+    Language language = new LanguageFacade().find(9);
+
+    System.out.print(language.getId() + " " + language.getName() + " ");
+    language.getNames().forEach(n -> {
+      System.out.print(n.getName() + " ");
+    });
+  }
 
 }
